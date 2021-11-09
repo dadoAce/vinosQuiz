599d3402c3c8519e2838bb4446cccba6ea15a339
Alfonso Acevedo <Eduardo_acevedo@grupoiikno.com>
2021-11-09 13:38:34 -0600

    ACTUALIZAR

--- Controlador/Home.php
+++ Controlador/Home.php
@@ -7,7 +7,8 @@ class Home extends App {
     }
 
     public function index() {
-
+        header('Access-Control-Allow-Origin: https://vinosdado.myshopify.com/');
+        header('Access-Control-Allow-Methods: GET'); 
         $arreglo = []; 
         $arreglo["inicio"][0]="busqueda";
         $arreglo["inicio"][1]="xx";
