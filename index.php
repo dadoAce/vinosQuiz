6b4f1055a634d7234a271a29a7339b2cd7df784c
Alfonso Acevedo <Eduardo_acevedo@grupoiikno.com>
2021-11-09 13:46:23 -0600

    a

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
  
- header('Access-Control-Allow-Origin: https://vinosdado.myshopify.com/');
- header('Access-Control-Allow-Methods: GET'); 
+ header('Access-Control-Allow-Origin: *'); 
 require_once 'libs/App.php'; 
 require_once 'libs/Modelo.php';
 $app = new App();
