668c00a68220acc283439ebe969d59a6a55cec92
Alfonso Acevedo <Eduardo_acevedo@grupoiikno.com>
2021-11-10 22:42:27 -0600

    cambios

---
+++ Controlador/Ticket.php
@@ -0,0 +1,21 @@
+<?php
+
+class Ticket extends App
+{
+
+    function __construct()
+    {
+    }
+
+    public function index()
+    {
+
+        $this->inicio();
+    }
+
+    public function inicio()
+    {/* Vista */
+ 
+        $this->vista("ticket/inicio",);
+    } 
+}
---
+++ Views/ticket/prueba.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Prueba</title>
+    <?php include_once 'Views/complementos/referencias/referencias.php'; ?>
+</head>
+
+<body class=" ">
+    <main class="main-principal h-100">
+
+        <?php $this->vista("complementos/menus/menuPrincipal"); ?>
+        <div id="contenedor">
+
+           
+        </div>
+
+    </main>
+
+</body>
+
+</html>
+
+<?php include_once 'Views/complementos/referencias/referencias_footer.php'; ?>;
