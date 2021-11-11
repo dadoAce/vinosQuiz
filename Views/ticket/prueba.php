214d872a3bce5edf14364622816e12f5fcd098e3
Alfonso Acevedo <Eduardo_acevedo@grupoiikno.com>
2021-11-10 22:45:33 -0600

    actualizar

--- Views/ticket/prueba.php
+++ Views/ticket/prueba.php
@@ -12,8 +12,103 @@
 
         <?php $this->vista("complementos/menus/menuPrincipal"); ?>
         <div id="contenedor">
+            <form method="post" action="http://50.246.39.154:9000/WebValidationManager/(S(r45klyhhywmayezowoea1wn3))/Default.aspx" id="DiscountSelection" class="visibledivstyle">
+                <div class="aspNetHidden">
+                    <input type="hidden" name="__LASTFOCUS" id="__LASTFOCUS" value="">
+                    <input type="hidden" name="__EVENTTARGET" id="__EVENTTARGET" value="">
+                    <input type="hidden" name="__EVENTARGUMENT" id="__EVENTARGUMENT" value="">
+                    <input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="/wEPDwUJMjcxNDA1MjI5DxYCHhNWYWxpZGF0ZVJlcXVlc3RNb2RlAgEWCgIDD2QWCAIDDxYCHglpbm5lcmh0bWwFFldlYiBWYWxpZGF0aW9uIE1hbmFnZXJkAgUPFgIfAQUGTG9nb3V0ZAIJDxYCHgdWaXNpYmxlaBYGAgEPDxYGHghDc3NDbGFzcwUXaGVhZGVybWVudWl0ZW0gc2VsZWN0ZWQeBFRleHQFBEhvbWUeBF8hU0ICAmRkAgMPDxYGHwMFDmhlYWRlcm1lbnVpdGVtHwQFDkFkbWluaXN0cmF0aW9uHwUCAmRkAgUPDxYGHwMFDmhlYWRlcm1lbnVpdGVtHwQFEERpc2NvdW50IGhpc3RvcnkfBQICZGQCCw8WAh8CaBYCAgEPDxYCHwRlZGQCBQ8WAh8BBQ9Bc3NpZ24gRGlzY291bnRkAgkPZBYSAgEPDxYCHwQFC0Nvc3QgY2VudHJlZGQCBQ8PFgIfAmhkZAIHDxAPFgYeDURhdGFUZXh0RmllbGQFCE9EU19OYW1lHg5EYXRhVmFsdWVGaWVsZAUHT0RTX1VJZB4LXyFEYXRhQm91bmRnZBAVAwlEYWlseSBNYXgNRG9sbGFyIEFtb3VudA9GdWxsIFZhbGlkYXRpb24VAyRkOWRjOTMxMi1jZDMzLTQwNDEtYmMyNi02OTQ5MGUyMDRkOGUkZTcyNWE2NDktZGU3Mi00ZDU5LWI5ZDgtNzQ5Yzc0OGFkNDAwJDQ5Mjc5ZjVhLTlmMzctNDhhMi05ZTZjLWUxMDI0YTY1ODdmORQrAwNnZ2cWAWZkAgkPDxYCHwQFDkRpc2NvdW50IGxldmVsZGQCDQ8QDxYGHwcFBFN0ZXAfBgUSVGV4dFJlcHJlc2VudGF0aW9uHwhnZBAVAQg4LDAwIOKCrBUBATEUKwMBZ2RkAg8PDxYCHwQFClRpY2tldCBuby5kZAITDw8WAh8EBQdDb21tZW50ZGQCFw8QDxYCHwQFBEhpc3RkZGRkAhkPDxYCHwQFBFNlbmRkZAILDw8WBB8DBRZ2aXNpYmxlZGl2c3R5bGUgaGlkZGVuHwUCAmRkAg0PZBYEAgEPFgIfAQUHSW1wcmludGQCAw8WAh8BBQpUaWNrZXQgbm8uZBgBBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WAQUQY2hlY2tIaXN0b3J5T25seQdSG8qMr5BmJh9A8MgNyl2AntRCWEiSFvS2k9tXITWy">
+                </div>
 
-           
+                <script type="text/javascript">
+                    //<![CDATA[
+                    var theForm = document.forms['DiscountSelection'];
+                    if (!theForm) {
+                        theForm = document.DiscountSelection;
+                    }
+
+                    function __doPostBack(eventTarget, eventArgument) {
+                        if (!theForm.onsubmit || (theForm.onsubmit() != false)) {
+                            theForm.__EVENTTARGET.value = eventTarget;
+                            theForm.__EVENTARGUMENT.value = eventArgument;
+                            theForm.submit();
+                        }
+                    }
+                    //]]>
+                </script>
+
+
+                <script src="/WebValidationManager/WebResource.axd?d=rlsYYpeXHTsP4EBoSGEQ37aXj3s-RxwAjLpVSQr6AAGkFuV0UeyuFPnmltuZfH3bFQnVGFSlQ0q63xDrJWLhG4t9gCBAEFwPqPkk2UVTH_E1&amp;t=636893357460000000" type="text/javascript"></script>
+
+
+                <script src="/WebValidationManager/WebResource.axd?d=Nb5C_jbTkgGCoirEnb_2xCNp8WSYXc3N82x6UbTDuzD5zw0028D6PopZroJ7NSmG3y-LxEjmscMEobq7vwe6qUHdAHHIRtk0HcaF4AyWDNI1&amp;t=636893357460000000" type="text/javascript"></script>
+                <div class="aspNetHidden">
+
+                    <input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="5B42B052">
+                    <input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="/wEdAAwZWVaDdSLbNFeziSVneUFPI92hPpUOsk2wqZGonf3O2cVwX8PBzUjmYok9d2X2fk5FxZbHOnPA7I0tFTCmL597/BWep2YIhYYC2OvlK7ptL3wv9abcrnxAHXM9PX1HRHt6Im+V0oCNcGkFPHmnzQhBfE5pQy+rkkff5HY97cnw3MYracGjYWl8NqRJJVSdsLBfkYBxDTXk+WuMJEGi/s/+rbjQ7JWh6eOKkaj5v/wiJV9LQkT0ijMJXzI+9BZOSlS3gkr8qTGL9whzrj/zYaUuvWmgpqGEPWVENDC69nsbEQ==">
+                </div>
+
+                <fieldset>
+                    <legend>Contact Information</legend>
+
+                    <label for="ddlistODS" id="lblCostUnit" class="Label">Cost centre</label>
+
+
+                    <!--<div id="combobox_container" class="ui-widget" style="float:right; margin-right:30px;">                
+            </div>-->
+
+
+
+
+
+                    <select name="ddlistODS" onchange="javascript:setTimeout('__doPostBack(\'ddlistODS\',\'\')', 0)" id="ddlistODS" class="InputControls dropdownlist">
+                        <option selected="selected" value="d9dc9312-cd33-4041-bc26-69490e204d8e">Daily Max</option>
+                        <option value="e725a649-de72-4d59-b9d8-749c748ad400">Dollar Amount</option>
+                        <option value="49279f5a-9f37-48a2-9e6c-e1024a6587f9">Full Validation</option>
+
+                    </select>
+                    <br class="clear">
+
+                    <br>
+                    <span id="lblDiscount" class="Label">Discount level</span>
+
+
+
+                    <select name="listODSKeys" onchange="javascript:setTimeout('__doPostBack(\'listODSKeys\',\'\')', 0)" id="listODSKeys" class="InputControls">
+                        <option selected="selected" value="1">8,00 €</option>
+
+                    </select>
+
+                    <br class="clear">
+                    <br>
+                    <label for="txtCardNr" id="lblCardNo" class="Label">Ticket no.</label>
+                    <input name="txtCardNr" type="text" value="321" maxlength="20" id="txtCardNr" class="InputControls  textbox" style="width:200px;">
+                    <br class="clear">
+                    <br>
+
+                    <label for="txtComment" id="lblComment" class="Label">Comment</label>
+                    <textarea name="txtComment" rows="2" cols="20" id="txtComment" class="InputControls  textbox" style="height:100px;width:200px;overflow:auto;"></textarea>
+                    <br class="clear">
+                    <br>
+
+                    <!--<input id="checkHistoryOnly" type="checkbox" name="checkHistoryOnly" /><label for="checkHistoryOnly">Hist</label>
+            <br class="clear" />
+            <br />-->
+                    <input type="submit" name="btnSendODSRequest" value="Send" id="btnSendODSRequest" class="button">
+                    <br>
+                    <br>
+                    <span id="lblDiscountLimit"></span>
+                    <br>
+                </fieldset>
+
+
+
+                <script type="text/javascript">
+                    //<![CDATA[
+                    WebForm_AutoFocus('txtCardNr'); //]]>
+                </script>
+            </form>
+
         </div>
 
     </main>
