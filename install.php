8abf094e3647ff0e466083f6ed5a3f8f7243e0d5
Alfonso Acevedo <Eduardo_acevedo@grupoiikno.com>
2021-11-25 15:49:29 -0600

    h

--- install.php
+++ install.php
@@ -1,24 +1,2 @@
 <?php
-
-// Set variables for our request
-$shop = $_GET['shop'];
-$api_key = "46c7a89d69acccff9847def5eb0346af";
-$scopes = "read_orders,write_products";
-
-/* 
-**/
-$scopes = "read_orders,read_products";
-
-$redirect_uri = "https://www.dadoroom.com/vinos/generate_token.php";
-
-// Build install/approval URL to redirect to
-$install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
-
-try {
-    
-    // Redirect
-    header("Location: " . $install_url);
-    die();
-} catch (\Throwable $th) {
-    echo var_dump($th);
-}
+echo "hola";
