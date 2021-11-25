1bd5857252814f4fdcf821c7c80743d15c2f38f5
Alfonso Acevedo <Eduardo_acevedo@grupoiikno.com>
2021-11-25 15:50:55 -0600

    asd

--- generate_token.php
+++ generate_token.php
@@ -1,49 +1,52 @@
 <?php
+try {
 
-// Get our helper functions
-require_once("inc/functions.php");
-
-// Set variables for our request
-$api_key = "46c7a89d69acccff9847def5eb0346af";
-$shared_secret = "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";
-$params = $_GET; // Retrieve all request parameters
-$hmac = $_GET['hmac']; // Retrieve HMAC request parameter
-
-$params = array_diff_key($params, array('hmac' => '')); // Remove hmac from params
-ksort($params); // Sort params lexographically
-
-$computed_hmac = hash_hmac('sha256', http_build_query($params), $shared_secret);
-
-// Use hmac data to check that the response is from Shopify or not
-if (hash_equals($hmac, $computed_hmac)) {
+	// Get our helper functions
+	require_once("inc/functions.php");
 
 	// Set variables for our request
-	$query = array(
-		"client_id" => $api_key, // Your API key
-		"client_secret" => $shared_secret, // Your app credentials (secret key)
-		"code" => $params['code'] // Grab the access key from the URL
-	);
+	$api_key = "46c7a89d69acccff9847def5eb0346af";
+	$shared_secret = "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";
+	$params = $_GET; // Retrieve all request parameters
+	$hmac = $_GET['hmac']; // Retrieve HMAC request parameter
 
-	// Generate access token URL
-	$access_token_url = "https://" . $params['shop'] . "/admin/oauth/access_token";
+	$params = array_diff_key($params, array('hmac' => '')); // Remove hmac from params
+	ksort($params); // Sort params lexographically
 
-	// Configure curl client and execute request
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_URL, $access_token_url);
-	curl_setopt($ch, CURLOPT_POST, count($query));
-	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query));
-	$result = curl_exec($ch);
-	curl_close($ch);
+	$computed_hmac = hash_hmac('sha256', http_build_query($params), $shared_secret);
 
-	// Store the access token
-	$result = json_decode($result, true);
-	$access_token = $result['access_token'];
+	// Use hmac data to check that the response is from Shopify or not
+	if (hash_equals($hmac, $computed_hmac)) {
 
-	// Show the access token (don't do this in production!)
-	echo $access_token;
+		// Set variables for our request
+		$query = array(
+			"client_id" => $api_key, // Your API key
+			"client_secret" => $shared_secret, // Your app credentials (secret key)
+			"code" => $params['code'] // Grab the access key from the URL
+		);
 
-} else {
-	// Someone is trying to be shady!
-	die('This request is NOT from Shopify!');
+		// Generate access token URL
+		$access_token_url = "https://" . $params['shop'] . "/admin/oauth/access_token";
+
+		// Configure curl client and execute request
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $access_token_url);
+		curl_setopt($ch, CURLOPT_POST, count($query));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query));
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+		// Store the access token
+		$result = json_decode($result, true);
+		$access_token = $result['access_token'];
+
+		// Show the access token (don't do this in production!)
+		echo $access_token;
+	} else {
+		// Someone is trying to be shady!
+		die('This request is NOT from Shopify!');
+	}
+} catch (\Throwable $th) {
+	echo var_dump($th);
 }
